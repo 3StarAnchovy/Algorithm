d8db99891202d9467484b5ef3ed2bb16cb956d7a
Hong Ji Min <baker0929@naver.com>
2023-01-25 22:33:54 +0900

    fix 14400

--- algorithm/untitled/src/brute_force/boj_14400.java
+++ algorithm/untitled/src/brute_force/boj_14400.java
@@ -1,5 +1,6 @@
 package brute_force;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class boj_14400 {
@@ -8,9 +9,26 @@ public class boj_14400 {
         Scanner scanner = new Scanner(System.in);
 
         int n = scanner.nextInt();
+        int[] x = new int[n];
+        int[] y = new int[n];
+
+        //input
         for(int i = 0; i < n; i ++)
         {
-
+            x[i] = scanner.nextInt();
+            y[i] = scanner.nextInt();
         }
+
+        Arrays.sort(x);
+        Arrays.sort(y);
+        //중앙값 추출
+        int mid_x = x[n / 2];
+        int mid_y = y[n / 2];
+
+        int sum = 0;
+        for(int i = 0; i < n; i ++)
+            sum += Math.abs(mid_x - x[i]) + Math.abs(mid_y - y[i]);
+
+        System.out.println(sum);
     }
 }
