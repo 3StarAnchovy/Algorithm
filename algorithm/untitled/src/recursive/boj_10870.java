81e95d7dfe189077d1654d45c606440b56c35eaa
홍지민 <baker0929@naver.com>
2023-01-28 16:49:05 +0900

    add 10870

--- algorithm/untitled/src/recursive/boj_10870.java
+++ algorithm/untitled/src/recursive/boj_10870.java
@@ -1,2 +1,20 @@
-package recursive;public class boj_10870 {
+package recursive;
+
+import java.util.Scanner;
+
+public class boj_10870 {
+    public static int recursive(int n)
+    {
+        if(n >= 2)
+            return recursive(n - 1) + recursive(n - 2);
+        return n;
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        System.out.println(recursive(n));
+    }
 }
