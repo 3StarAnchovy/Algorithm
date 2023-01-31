38ed94ca9c255f477b7cdce3fc570d729dc93908
홍지민 <baker0929@naver.com>
2023-01-31 16:41:25 +0900

    fix hanoi

---
+++ algorithm/untitled/src/recursive/boj_11729.java
@@ -0,0 +1,25 @@
+package recursive;
+
+import java.util.Scanner;
+
+public class boj_11729 {
+    public static void recursive(int n, int start, int dep, int mid)
+    {
+        if(n != 1)
+        {
+            recursive(n - 1, start, mid, dep);
+            System.out.println(start + " " + dep);
+            recursive(n - 1,mid, dep, start);
+            return;
+        }
+        System.out.println(start + " " + dep);
+    }
+
+    public static void main(String[] args)
+    {
+        //recursive(n) = recursive(n - 1) + 1;
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        recursive(n,1,3,2);
+    }
+}
