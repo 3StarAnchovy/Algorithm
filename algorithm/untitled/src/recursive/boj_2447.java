f64ca06bc8e01e6fe5b20f2f8c776832d5ccd5ca
홍지민 <baker0929@naver.com>
2023-02-01 16:24:56 +0900

    fix

---
+++ algorithm/untitled/src/recursive/boj_2447.java
@@ -0,0 +1,18 @@
+package recursive;
+
+import java.util.Scanner;
+
+public class boj_2447 {
+    public static void recursive(int N, int i, int j)
+    {
+
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+
+        recursive(N,0,0);
+    }
+}
