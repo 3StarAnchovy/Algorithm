84ad943a942cb4a2c61e77304a14327e054222c7
홍지민 <baker0929@naver.com>
2023-02-06 21:54:33 +0900

    um..

--- algorithm/untitled/src/brute_force/boj_7696.java
+++ algorithm/untitled/src/brute_force/boj_7696.java
@@ -3,45 +3,38 @@ package brute_force;
 import java.util.Scanner;
 
 public class boj_7696 {
-    public static boolean checkDigit(int n, int temp)
+    public static boolean isDouble(int num)
     {
-        while(n != 0)
+        int[] cnt = new int[10];
+        while(num != 0)
         {
-            if((n % 10) != temp)
+            cnt[num % 10] ++;
+            if(cnt[num % 10] != 1)
                 return false;
-            n /= 10;
+            num /= 10;
         }
-        System.out.println("true");
+
         return true;
     }
     public static void main(String[] args)
     {
         Scanner scanner = new Scanner(System.in);
-        //tt
-        int n = 1;
+
+        int n = scanner.nextInt();
+
         while(n != 0)
         {
-            n = scanner.nextInt();
-            int cnt = 0;
-            int sum = 0;
-            for(int i = 1; i <=n; i ++)
+            int cnt;
+            //시행 횟수
+            cnt = n;
+            for(int i = 1; i <= n; i ++)
             {
-                if(i < 10)
-                {
+                //true면 중복아닌
+                if(!isDouble(i))
                     cnt ++;
-                    continue;
-                }
-                //자릿수가 같지 않으면
-                if(checkDigit(cnt, cnt % 10) == false)
-                    cnt ++;
-                else {
-                    cnt ++;
-                    sum++;
-                }
             }
-            if (n == 0)
-                break;
-            System.out.println(cnt + sum);
+            System.out.println(cnt);
+            n = scanner.nextInt();
         }
     }
 }
