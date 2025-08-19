b92bd377ead4ceaaffbd648d099a403dda322424
홍지민 <baker0929@naver.com>
2025-08-19 21:00:13 +0900

    feat : 에잉 몰라

---
+++ algorithm/untitled/src/samsung_2025/순조부/boj_15650_nm2.java
@@ -0,0 +1,42 @@
+package samsung_2025.순조부;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_15650_nm2 {
+    static int N,M;
+    static int[] picked;
+    static int[] arr;
+    static boolean[] visited;
+    static StringBuilder sb;
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        picked = new int[M];
+        visited = new boolean[N];
+        sb = new StringBuilder();
+
+        dfs(0,0);
+        System.out.print(sb);
+    }
+
+    private static void dfs(int cnt, int start) {
+        if(cnt == M) {
+            for(int i = 0; i < M; i ++)
+                sb.append(picked[i]).append(" ");
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = start; i < N; i ++) {
+                picked[cnt] = i + 1;
+                dfs(cnt + 1, i + 1);
+        }
+    }
+}
---
+++ algorithm/untitled/src/samsung_2025/순조부/boj_15651_nm3.java
@@ -0,0 +1,45 @@
+package samsung_2025.순조부;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_15651_nm3 {
+    static int N,M;
+    static int[] arr;
+    static int[] picked;
+    static boolean[] visited;
+    static StringBuilder sb;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        picked = new int[M];
+        visited = new boolean[N];
+        sb = new StringBuilder();
+        dfs(0);
+        System.out.print(sb);
+    }
+
+    private static void dfs(int cnt) {
+        if(cnt == M) {
+            for(int i = 0; i < M; i ++)
+                sb.append(picked[i]).append(" ");
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = 0; i < N; i ++) {
+            if(!visited[i]) {
+                visited[i] = true;
+                picked[cnt] = i + 1;
+                dfs(cnt + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
